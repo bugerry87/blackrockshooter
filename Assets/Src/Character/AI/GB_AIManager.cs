ec4b4b5f8fee2fea3c7882dc4b20d51828d862df
Gerald Baulig <gerald.baulig@gmail.com>
2016-03-21 21:25:47 +0100

    commit all

---
+++ Assets/Src/Character/AI/GB_AIManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using GBAssets.Utils;
+
+namespace GBAssets.Character.AI
+{
+	public class GB_AIManager : ScriptableObject
+	{
+		public const string ATTACK = "ATTACK";
+
+		public void Request(GB_AI ai, string action)
+		{
+			switch (action)
+			{
+				case ATTACK: break;
+				default: break;
+			}
+		}
+
+
+	}
+}
---
+++ Assets/Src/Character/RPG/GB_RPGStates.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace GBAssets.Character.RPG
+{
+
+	public class GB_RPGStates : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+
+}
